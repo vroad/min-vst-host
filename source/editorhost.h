2121d21395244316f492d1ff28dcd197f0c49c80
vroad <396351+vroad@users.noreply.github.com>
2025-07-06 13:11:11 +0900

    chore: apply clang-format fixes

--- source/editorhost.h
+++ source/editorhost.h
@@ -11,8 +11,8 @@
 // LICENSE
 // (c) 2024, Steinberg Media Technologies GmbH, All Rights Reserved
 //-----------------------------------------------------------------------------
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
 //   * Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
@@ -23,16 +23,17 @@
 //     contributors may be used to endorse or promote products derived from this
 //     software without specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 //-----------------------------------------------------------------------------
 
 #pragma once
@@ -52,30 +53,29 @@ namespace EditorHost {
 class WindowController;
 
 //------------------------------------------------------------------------
-class App : public IApplication
-{
+class App : public IApplication {
 public:
-	~App () noexcept override;
-	void init (const std::vector<std::string>& cmdArgs) override;
-	void terminate () override;
+  ~App() noexcept override;
+  void init(const std::vector<std::string> &cmdArgs) override;
+  void terminate() override;
 
 private:
-	enum OpenFlags
-	{
-		kSetComponentHandler = 1 << 0,
-		kSecondWindow = 1 << 1,
-	};
-	void openEditor (const std::string& path, VST3::Optional<VST3::UID> effectID, uint32 flags);
-	void createViewAndShow (IEditController* controller);
+  enum OpenFlags {
+    kSetComponentHandler = 1 << 0,
+    kSecondWindow = 1 << 1,
+  };
+  void openEditor(const std::string &path, VST3::Optional<VST3::UID> effectID,
+                  uint32 flags);
+  void createViewAndShow(IEditController *controller);
 
-	VST3::Hosting::Module::Ptr module {nullptr};
-	IPtr<PlugProvider> plugProvider {nullptr};
-	Vst::HostApplication pluginContext;
-	WindowPtr window;
-	std::shared_ptr<WindowController> windowController;
+  VST3::Hosting::Module::Ptr module{nullptr};
+  IPtr<PlugProvider> plugProvider{nullptr};
+  Vst::HostApplication pluginContext;
+  WindowPtr window;
+  std::shared_ptr<WindowController> windowController;
 };
 
 //------------------------------------------------------------------------
-} // EditorHost
-} // Vst
-} // Steinberg
+} // namespace EditorHost
+} // namespace Vst
+} // namespace Steinberg
